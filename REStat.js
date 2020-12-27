fdc712b57f0ed0df825b604f93b21cd3183114a4
KennHuang <kennhung@users.noreply.github.com>
2020-12-27 17:05:40 +0800

    wa

--- REStat.js
+++ REStat.js
@@ -7,6 +7,59 @@ const authHeaders = {
     Authorization: `Basic ${process.env.DJ_TOKEN}`
 };
 
+const getTeamsStat = (teams, submissions, judgements, type) => {
+    const processed = submissions.map(({ id, team_id }) => {
+        const d = judgements.filter(({ submission_id }) => {
+            return submission_id === id;
+        });
+
+        if (d.findIndex(({ judgement_type_id }) => judgement_type_id === "AC") !== -1) {
+            return {
+                team_id,
+                judgement_type_id: "AC"
+            }
+        } else {
+            if (d.length === 0) {
+                console.log(id, team_id, d);
+
+                return {
+                    team_id,
+                    judgement_type_id: "ERR"
+                }
+            }
+
+            return {
+                team_id,
+                judgement_type_id: d[d.length - 1].judgement_type_id
+            }
+        }
+    }).filter(({ judgement_type_id }) => judgement_type_id === type);
+
+
+    let total = 0;
+
+
+    const stat = teams.map((t) => {
+        const { name, externalid, id } = t;
+        const count = processed.filter(({ team_id }) => team_id === id).length;
+
+        total += count;
+
+        return {
+            name,
+            externalid,
+            count
+        }
+    }).filter(({ count }) => count > 0).sort((a, b) => {
+        return b.count - a.count;
+    });
+
+    return {
+        stat,
+        total
+    };
+}
+
 (async () => {
 
 
@@ -30,33 +83,7 @@ const authHeaders = {
 
 
     const stat = (type) => {
-
-        const processed = submissions.map(({ id, team_id }) => {
-            const d = judgements.filter(({ submission_id }) => {
-                return submission_id === id;
-            });
-
-            if (d.findIndex(({ judgement_type_id }) => judgement_type_id === "AC") !== -1) {
-                return {
-                    team_id,
-                    judgement_type_id: "AC"
-                }
-            } else {
-                if (d.length === 0) {
-                    console.log(id, team_id, d);
-
-                    return {
-                        team_id,
-                        judgement_type_id: "ERR"
-                    }
-                }
-
-                return {
-                    team_id,
-                    judgement_type_id: d[d.length - 1].judgement_type_id
-                }
-            }
-        }).filter(({ judgement_type_id }) => judgement_type_id === type);
+        const { stat, total } = getTeamsStat(teams, submissions, judgements, type);
 
         new Line()
             .padding(2)
@@ -65,21 +92,7 @@ const authHeaders = {
             .column('count', 10, [chalk.cyan])
             .output();
 
-
-        let total = 0;
-        teams.map((t) => {
-            const { name, externalid, id } = t;
-            const count = processed.filter(({ team_id }) => team_id === id).length;
-
-            return {
-                name,
-                externalid,
-                count
-            }
-        }).filter(({ count }) => count > 0).sort((a, b) => {
-            return b.count - a.count;
-        }).forEach(({ externalid, name, count }) => {
-            total += count;
+            stat.forEach(({ externalid, name, count }) => {
             new Line()
                 .padding(2)
                 .column(externalid || "n/a", 15, [chalk.grey])
